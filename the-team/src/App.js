483619d224a378ed2dc81f2930f02227f150333a
Wesley-Ryan <im.wesleyryan@gmail.com>
2020-10-13 10:59:55 -0700

    Starting to build out form

--- the-team/src/App.js
+++ the-team/src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
+import TeamForm from './components/TeamForm'
 import './App.css';
 
 function App() {
+  const initialList = [{ 
+    name: "Wesley ", 
+    email: "wesley-white@lambdaScool.com ", 
+    role: "Full Stack Developer"
+  }]
+  const [teamList, setTeamList] = useState(initialList)
   return (
     <div className="App">
       <h1>The Team</h1>
+      <TeamForm />
     </div>
   );
 }
--- the-team/src/components/TeamForm.js
+++ the-team/src/components/TeamForm.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import '../App.css'
 
 
 const TeamForm = () => {
     return ( 
-    <div className="form-container">
-        <form>
-            <input />
-            <input />
-            <input />
+    <div >
+        <form className="form-container" >
+            <div className="input-container">  
+            Name:  <input />
+            </div>
+            <div className="input-container">
+            Email: <input />
+            </div>
+            <div className="input-container"> 
+            Role: <input />
+            </div>    
         </form>
     </div> );
 }
