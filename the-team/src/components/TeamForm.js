45b92d6e9b4119bfb8303d7c5419b51b78f24e91
Wesley-Ryan <im.wesleyryan@gmail.com>
2020-10-13 10:43:19 -0700

    Cleaned up App, added two components

---
+++ the-team/src/components/TeamForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const TeamForm = () => {
+    return ( 
+    <div className="form-container">
+        <form>
+            <input />
+            <input />
+            <input />
+        </form>
+    </div> );
+}
+ 
+export default TeamForm;
---
+++ the-team/src/components/TeamList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+const TeamList = (props) => {
+    const {team} = props
+    return (
+        <div className='team-container'>
+
+        </div>
+      );
+}
+ 
+export default TeamList;
